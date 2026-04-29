1d317bfcd71529126dbd0362631479a34f9e281b
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-04-29 19:21:41 +0530

    js loops

---
+++ javaloops/script.js
@@ -0,0 +1,22 @@
+// for(var i=1;i<=10;i++)
+// {
+//    var a=2;
+//    console.log(a*i);
+// }
+// let b=[24,34,26,31,56,78,87,23,12,100];
+// for(var k=0;k<b.length;k++)
+// {
+//     console.log(b[k]);
+// }
+// let j=0;
+// while(j<b.length)
+// {
+//     console.log(b[j]);
+// }
+let j=1;
+while(j<=10)
+{
+    var c=2;
+    console.log(c*i);
+    j++;
+}
