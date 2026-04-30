6c76dd99290bd3e0e2c4d1c043bcea8cc9d47876
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-04-30 19:46:33 +0530

    conditional statement in javascript

---
+++ conditional statement java scrpit/script.js
@@ -0,0 +1,51 @@
+// var num=88;
+// if(num>=90)
+// {
+//     console.log(A);
+// }
+// else if(num>75&&num<90)
+// {
+//     console.log(B);
+// }
+// else if(num>50&&num<75)
+// {
+//   console.log(C);
+// }
+// else
+// {
+//      console.log("fail");
+// }
+
+// let username = "anshika";
+// let pas = "12345";
+
+// if (username === "anshika" && pas === "12345") {
+//   console.log("login success");
+// } else {
+//   console.log("failed");
+// }
+
+// let data;
+// let Name="anshika";
+
+// data = Name ||"N/A"
+// console.log(data);
+
+let choice;
+switch (choice) {
+  case 1: {
+    console.log("tea");
+    break;
+  }
+  case 1: {
+    console.log("coffee");
+    break;
+  }
+  case 1: {
+    console.log("lassi");
+    break;
+  }
+  default: {
+    console.log("paani");
+  }
+}
---
+++ javaloops/script2.js
@@ -0,0 +1,116 @@
+// var i=1;
+// while(i<=5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// var i=5;
+// while(i>=1)
+// {
+//     console.log(i);
+//     i--;
+// }
+
+// var i=2;
+// while(i<11)
+// {
+//     console.log(i);
+//     i=i+2;
+// }
+
+// var i=10;
+// var sum=0;
+// while(i>=5)
+// {
+    
+//    sum+=i;
+//    i--;
+
+// }
+// console.log(sum);
+
+// var i = 1;   // initialization
+
+// while (i <= 5) {   // condition
+//     console.log(i);
+//     i++;           // increment
+// }
+
+// var i = 1;   // initialization
+
+// while (i <= 5) {   // condition
+//     console.log(i);//work
+//     i++;           // increment
+// }
+
+// var i = 10;   // start value
+
+// while (i < 5)   // condition check → 10 < 5  false
+// {
+//     console.log(i);   // ye kabhi execute hi nahi hoga
+//     i++;
+// }
+
+// var i = 1;   // start
+
+// while (i <= 5)   // condition hamesha true
+// {
+//     console.log(i);   // 1 print hota rahega
+//     // i++ missing
+// }
+
+// // i change nahi hua
+// // condition kabhi false nahi hui
+// // infinite loop
+
+// var i = 1;   // start
+
+// while (i <= 5)
+// {
+//     console.log(i);
+//     i--;   // galat direction
+// }
+
+// // i = 1 → 0 → -1 → -2 ...
+// // condition (i <= 5) hamesha true
+// // infinite loop
+
+
+// var i = 1;
+
+// while (i <= 3)   // jab tak true tab tak chalega
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// // i = 1,2,3 tak chalega
+// // i = 4 hote hi condition false
+// // loop stop
+
+// var i = 1;
+
+// while (i <= 5)
+// {
+//     console.log(i);   // 1 2 3 4 5
+//     i++;   // aage badh raha hai
+// }
+
+// var i = 5;
+
+// while (i >= 1)
+// {
+//     console.log(i);   // 5 4 3 2 1
+//     i--;   // peeche aa raha hai
+// }
+
+// b 
+
+// i = 1 run
+// i = 2 run
+// i = 3 run
+// i = 4 run
+// i = 5 stop
+
+// total 4 baar chala
