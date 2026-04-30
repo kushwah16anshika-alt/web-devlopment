403a56deba70f7339caa2089f4cfd676fb918fcb
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-04-30 16:10:21 +0530

    assignment 1 loops

---
+++ javaloops/script1.js
@@ -0,0 +1,58 @@
+// for(var i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+// for(var i=1;i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+// for(var i=10;i<=1;i++)
+// {
+//     console.log(i);
+// }
+
+// for(var i=2;i<=10;i++)
+// {
+//     if(i%2==0)
+//     {
+//         console.log(i);
+//     }
+    
+// }
+
+// for(var i=1;i<=9;i++)
+// {
+//     if(i%2!=0)
+//     {
+//         console.log(i);
+//     }
+    
+// }
+
+// for(var i=1;i<=10;i++)
+// {
+//     console.log(15*i);
+// }
+
+// for(var i=1;i<=20;i+=2)
+// {
+    
+//         console.log(i);
+   
+    
+// }
+
+// for(var i=5;i<=50;i+=5)
+// {
+//         console.log(i);    
+// }
+
+for(var i=1;i<=10;i++)
+{
+        if(i%3==0)
+            {
+                console.log(i);  
+            }  
+}
