f387099452ed924b308e7fd46c0bc205cd42af7a
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-01 19:12:59 +0530

    methods in js

---
+++ jsmethods/script.js
@@ -0,0 +1,13 @@
+let a = 20,
+  b = 30;
+
+function sum(x, y) {
+  let z = x + y;
+
+  return z;
+}
+sum(a, b);
+
+function welcome() {
+  console.log("hello");
+}
---
+++ jsmethods/script1.js
@@ -0,0 +1,10 @@
+let abc = function () {
+  console.log("hello");
+};
+console.log(abc);
+
+let add = function (a, b) {
+  return a + b;
+};
+console.log(add);
+console.log(add(5, 5));
