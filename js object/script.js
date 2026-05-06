de1c4fd31280356f517df90171d7580061a2bc71
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-06 20:22:14 +0530

    js object

---
+++ js object/script.js
@@ -0,0 +1,40 @@
+let Fdata = [
+  {
+    fullname: "Anshika Kushwah",
+    phone: "9876543210",
+    city: "Bhopal",
+    profession: "Student",
+  },
+  {
+    fullname: "Rahul Sharma",
+    phone: "9123456780",
+    city: "Indore",
+    profession: "Engineer",
+  },
+  {
+    fullname: "Priya Verma",
+    phone: "9988776655",
+    city: "Delhi",
+    profession: "Teacher",
+  },
+  {
+    fullname: "Amit Patel",
+    phone: "9012345678",
+    city: "Ahmedabad",
+    profession: "Doctor",
+  },
+];
+
+Fdata.forEach((element) => {
+  console.log(element);
+});
+
+let anshika={
+    fullname: "Anshika Kushwah",
+    phone: "9876543210",
+    city: "Bhopal",
+    profession: "Student",
+}
+console.log(Object.keys(anshika));
+console.log(Object.values(anshika));
+
