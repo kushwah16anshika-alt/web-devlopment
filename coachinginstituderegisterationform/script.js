5049df46d0b0e14183566d2f832288afac60269d
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-16 01:46:43 +0530

    validation update

--- coachinginstituderegisterationform/script.js
+++ coachinginstituderegisterationform/script.js
@@ -1,13 +1,13 @@
-document
+   document
   .getElementById("registrationForm")
   .addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const fullname = document.getElementById("fullname").value;
+    const fullname = document.getElementById("fullname").value.trim();
 
-    const email = document.getElementById("email").value;
+    const email = document.getElementById("email").value.trim();
 
-    const number = document.getElementById("number").value;
+    const number = document.getElementById("number").value.trim();
 
     const dob = document.getElementById("dateofbirth").value;
 
@@ -31,51 +31,33 @@ document
       timings.push(item.value);
     });
 
-    const address = document.getElementById("address").value;
+    const address = document.getElementById("address").value.trim();
 
-    const city = document.getElementById("city").value;
+    const city = document.getElementById("city").value.trim();
 
-    const pincode = document.getElementById("pincode").value;
+    const pincode = document.getElementById("pincode").value.trim();
 
-    const registrationdata=
-    {
-      "Full Name:": fullname,
-      "Email:": email,
-      "Phone Number:": number,
-      "Date of Birth:": dob,
-      "Gender:": gender,
-      "Qualification:": qualification,
-      "Grade:": grade,
-      "Course:": course,
-      "Timings:": timings,
-      "Address:": address,
-      "City:":city,
-      "Pincode:": pincode
-    }
+    const registrationdata = {
+      "Full Name": fullname,
+      Email: email,
+      "Phone Number": number,
+      "Date of Birth": dob,
+      Gender: gender,
+      Qualification: qualification,
+      Grade: grade,
+      Course: course,
+      Timings: timings,
+      Address: address,
+      City: city,
+      Pincode: pincode,
+    };
 
-    console.log("Full Name:", fullname);
+    console.log(registrationdata);
 
-    console.log("Email:", email);
-
-    console.log("Phone Number:", number);
-
-    console.log("Date of Birth:", dob);
-
-    console.log("Gender:", gender);
-
-    console.log("Qualification:", qualification);
-
-    console.log("Grade:", grade);
-
-    console.log("Course:", course);
-
-    console.log("Timings:", timings);
-
-    console.log("Address:", address);
-
-    console.log("City:", city);
-
-    console.log("Pincode:", pincode);
+    validateData(registrationdata)
+      ? (console.log("Registration Data:", registrationdata),
+        alert("Registration successful!"))
+      : alert("Registration unsuccessful! Enter valid data.");
   });
 
 document
@@ -85,7 +67,46 @@ document
 
     window.location.reload();
   });
-function vaildatedata(data)
-{
-  let isvalid=true;
+
+function validateData(data) {
+  document.querySelectorAll(".manualError").forEach((item) => {
+    item.innerText = "";
+  });
+
+  let isValid = true;
+
+  if (!data["Full Name"]) {
+    document.getElementById("fullnameError").innerText = "Full Name Required";
+    isValid = false;
+  } else if (!/^[A-Za-z\s]+$/.test(data["Full Name"])) {
+    document.getElementById("fullnameError").innerText =
+      "Only alphabets and spaces are allowed";
+    isValid = false;
+  }
+
+  if (
+    !/^[A-Za-z\d._]+@(gmail\.com|outlook\.com|ricr\.in)$/.test(data["Email"])
+  ) {
+    document.getElementById("emailError").innerText = "Enter valid email";
+    isValid = false;
+  }
+
+  if (!/^[6-9]\d{9}$/.test(data["Phone Number"])) {
+    document.getElementById("numberError").innerText =
+      "Only Indian mobile numbers allowed";
+    isValid = false;
+  }
+
+  if (!data["Gender"]) {
+    document.getElementById("genderError").innerText = "Please select gender";
+    isValid = false;
+  }
+
+  if (!/^\d{6}$/.test(data["Pincode"])) {
+    document.getElementById("pincodeError").innerText =
+      "Enter valid 6 digit pincode";
+    isValid = false;
+  }
+
+  return isValid;
 }
