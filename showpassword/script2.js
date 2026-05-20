e8f46020fccb2ef9ec25965291ee2481d82e346a
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-20 21:05:21 +0530

    changes in password manager's script 2

--- showpassword/script2.js
+++ showpassword/script2.js
@@ -14,17 +14,17 @@ function getDataFromLocalStorage() {
     return;
   }
 
-  data.forEach((element) => {
-    const TR = document.createElement("tr");
+  // data.forEach((element) => {
+  //   const TR = document.createElement("tr");
 
-    TR.innerHTML = `
-              <td>${element.WebsiteName}</td>
-              <td>${element.UserName}</td>
-              <td>${element.Password}</td>
-            `;
+  //   TR.innerHTML = `
+  //             <td>${element.WebsiteName}</td>
+  //             <td>${element.UserName}</td>
+  //             <td>${element.Password}</td>
+  //           `;
 
-    ShowData.appendChild(TR);
-  });
+  //   ShowData.appendChild(TR);
+  // });
 }
 
 getDataFromLocalStorage();
