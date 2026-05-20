6d4eb2409bd56907bd8e9c404cf51653b1dbd9b0
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-20 21:22:55 +0530

    new content added in PM SCRIPT2

--- showpassword/script2.js
+++ showpassword/script2.js
@@ -1,30 +1,72 @@
-function getDataFromLocalStorage() {
-  const data = JSON.parse(localStorage.getItem("Passwords")) || [];
+// function getDataFromLocalStorage() {
+//   const data = JSON.parse(localStorage.getItem("Passwords")) || [];
 
-  const ShowData = document.getElementById("passwordData");
+//   const ShowData = document.getElementById("passwordData");
 
-  console.log(data);
+//   console.log(data);
 
-  if (data.length <= 0) {
-    ShowData.innerHTML = `<tr>
-                <td colspan="3">
-                    No Password saved
-                </td>
-            </tr>`;
+//   if (data.length <= 0) {
+//     ShowData.innerHTML = `<tr>
+//                 <td colspan="3">
+//                     No Password saved
+//                 </td>
+//             </tr>`;
+//     return;
+//   }
+
+//   data.forEach((element) => {
+//     const TR = document.createElement("tr");
+
+//     TR.innerHTML = `
+//               <td>${element.WebsiteName}</td>
+//               <td>${element.UserName}</td>
+//               <td>${element.Password}</td>
+//             `;
+
+//     ShowData.appendChild(TR);
+//   });
+// }
+
+// getDataFromLocalStorage();
+
+
+
+
+
+
+
+
+
+function loadPasswords() {
+  const savedPasswords =
+    JSON.parse(localStorage.getItem("Passwords")) || [];
+
+  const tableBody = document.querySelector("#passwordData");
+
+  tableBody.innerHTML = "";
+
+  if (!savedPasswords.length) {
+    tableBody.innerHTML = `
+      <tr>
+        <td colspan="3">No Password Saved</td>
+      </tr>
+    `;
     return;
   }
 
-  // data.forEach((element) => {
-  //   const TR = document.createElement("tr");
+  for (let i = 0; i < savedPasswords.length; i++) {
+    const { WebsiteName, UserName, Password } = savedPasswords[i];
 
-  //   TR.innerHTML = `
-  //             <td>${element.WebsiteName}</td>
-  //             <td>${element.UserName}</td>
-  //             <td>${element.Password}</td>
-  //           `;
+    const row = `
+      <tr>
+        <td>${WebsiteName}</td>
+        <td>${UserName}</td>
+        <td>${Password}</td>
+      </tr>
+    `;
 
-  //   ShowData.appendChild(TR);
-  // });
+    tableBody.innerHTML += row;
+  }
 }
 
-getDataFromLocalStorage();
+loadPasswords();
