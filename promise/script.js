f73e4b6fd09677683f2ec20cc7528fcdd1ff2f51
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-22 21:35:26 +0530

    promise practice

--- promise/script.js
+++ promise/script.js
@@ -1,27 +1,49 @@
-async function getData(value) {
+// async function getData(value) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (value === 3) {
+//         reject("Query Not Found");
+//       } else {
+//         console.log("Data Sent", value);
+//         resolve("Query Solved");
+//       }
+//     }, 2000);
+//   });
+// }
+
+// // getData(1)
+// //   .then(() => getData(2))
+// //   .then(() => getData(3))
+// //   .then(() => getData(4))
+// //   .then(() => getData(5))
+// //   .catch((rej) => console.log(rej));
+
+// await getData(1)
+// await getData(2)
+// await getData(3)
+// await getData(4)
+// await getData(5)
+
+// Simple Promise Example
+function orderPizza() {
   return new Promise((resolve, reject) => {
+    let pizzaReady = true;
+
     setTimeout(() => {
-      if (value === 3) {
-        reject("Query Not Found");
+      if (pizzaReady) {
+        resolve("Pizza Delivered");
       } else {
-        console.log("Data Sent", value);
-        resolve("Query Solved");
+        reject("Pizza Cancelled");
       }
     }, 2000);
   });
 }
 
-// getData(1)
-//   .then(() => getData(2))
-//   .then(() => getData(3))
-//   .then(() => getData(4))
-//   .then(() => getData(5))
-//   .catch((rej) => console.log(rej));
-
-
-
-await getData(1)
-await getData(2)
-await getData(3)
-await getData(4)
-await getData(5)
+orderPizza()
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+console.log("Order Placed...");
