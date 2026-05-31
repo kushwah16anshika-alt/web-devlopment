cf6f66ac07e087c230cac90f54ede42f5b64fd0a
Anshika Kushwaha <kushwah16anshika@gmail.com>
2026-05-31 11:00:22 +0530

    write a first code in react in myreactapp

---
+++ frontend/myfirstreactapp/src/App.jsx
@@ -0,0 +1,19 @@
+import header from "./components/header";
+import footer from "./components/footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <div id="abcd">my first react app</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam neque
+        dicta illo, assumenda, nemo beatae, animi possimus facilis cumque fugit
+        omnis maiores perferendis. In cumque veritatis consequatur illo maiores
+        totam.
+      </p>
+
+      <Footer />
+    </>
+  );
+}
+export default App;
---
+++ frontend/myfirstreactapp/src/components/Header.jsx
@@ -0,0 +1,8 @@
+function header() {
+  return (
+    <>
+      <div id="header">this id my header</div>
+    </>
+  );
+}
+export default header;
